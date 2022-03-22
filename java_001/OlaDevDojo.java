5dd4e6fe6be4c4c3208245d2507132f0a1b389bc
Eric <erictomich@gmail.com>
2022-03-21 22:40:02 -0300

    first commit

---
+++ java_001/OlaDevDojo.java
@@ -0,0 +1,11 @@
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Ka-me-ha-me-ha");
+        mensagem();
+    }
+
+    public static void mensagem() {
+        System.out.println("Mensagem em uma funcao.");
+    }
+    
+}
