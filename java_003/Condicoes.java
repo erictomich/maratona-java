3becfce2f2129b7580cc2e7d1300a770ef990952
Eric <erictomich@gmail.com>
2022-03-22 23:13:06 -0300

    update

--- java_003/Condicoes.java
+++ java_003/Condicoes.java
@@ -1,6 +1,31 @@
 
 public class Condicoes {
     public static void main(String args[]) {
-        System.out.println("Teste");
+
+        int idade, tempoTrabalho, total;
+
+        idade = 35;
+        tempoTrabalho = 20;
+
+        Matematica mat = new Matematica();
+
+        total = Matematica.mat.somar(idade, tempoTrabalho);
+
+        if(total > 50) {
+            System.out.println("Maior que cinquenta.");
+        } else {
+            System.out.println("Menor que cinquenta.");
+        }
+    }
+
+    static int soma(int numA, int numB) {
+        return numA + numB;
+    }
+}
+
+class Matematica {
+    int fatorDivisao = 2;
+    public static int somar(int A, int B) {
+        return A + B;
     }
 }
