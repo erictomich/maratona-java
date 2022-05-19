c23995bacf4255e61e72e637779a644deb936e36
Eric <erictomich@gmail.com>
2022-05-19 17:34:29 -0300

    add examples

---
+++ java_anotacoes_internas/Anotacoes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Anotacoes {
+    public static void main(String args[]) {
+        
+        Stack<String> tarefas = new Stack<String>();
+
+        System.out.println("VERIFICAR PORQUE NAO ESTA EXIBINDO NEGATIVA");
+
+        tarefas.push("01 - Entrar no requerimento - no portal");
+
+        tarefas.push("02 - Verificar o requerimento específico");
+        
+        System.out.println(tarefas);
+
+    }
+}
